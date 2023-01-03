80695f692b521f292a62aa661fe1556cb0e12bca
profeandres <andrescantillo@hotmail.com>
2023-01-03 16:05:24 -0500

    cofig vercel json 3

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import app from "./server/app.js"
 import {PORT} from "./server/config.js"
 import {connectDB} from "./server/db.js"
 
-app.listen(PORT)
-console.log("server is listening at port: ",PORT);
+app.listen(PORT || 4000)
+console.log("server is listening at port: ", PORT);
 
 connectDB()
